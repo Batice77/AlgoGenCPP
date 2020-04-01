333d697b55ae004e98015bb31f2a541bae5b8901
Angèle <nicolas.besso@aliceadsl.fr>
2020-04-01 17:18:20 +0200

    SumsThreshold
    Critère d'arrêt pour regrouper tout les critères d'arrêts

--- AlgoGenCPP/SumsThreshold.h
+++ AlgoGenCPP/SumsThreshold.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <vector>
 
+template <typename T, typename I, typename Q, typename S>
 class SumsThreshold
 {
 public:
-	SumsThreshold()
-	{
-	}
+	SumsThreshold(const I& it, const Q& qual, const S& stab);
 	~SumsThreshold() {};
 
 	bool operator()(const std::vector<float> qualityList) {
-		return false;
+		return iterative(qualityList) || quality(qualityList) || stability(qualityList);
 	}
+
+protected:
+	const I& iterative;
+	const Q& quality;
+	const S& stability;
 };
+
+template<typename T, typename I, typename Q, typename S >
+inline SumsThreshold<T, I, Q, S>::SumsThreshold(const I& it, const Q& qual, const S& stab):iterative(it), quality(qual), stability(stab) {};
 
