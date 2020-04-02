dcc22d3d72f062de6adf5bbbafc049e5e78c7132
Florian CHAMPAUD <fchampaud@kyriopolis.fr>
2020-04-03 00:38:44 +0200

    Traveling Salesman Generator: rename population
    Also print generated solution

--- AlgoGenCPP_Test/TravelingSalesmanProblemGenerator.cpp
+++ AlgoGenCPP_Test/TravelingSalesmanProblemGenerator.cpp
@@ -2,6 +2,7 @@
 #include <numeric>
 #include <algorithm>
 #include <random>
+#include <iostream>
 
 #include "TravelingSalesmanProblemGenerator.h"
 
@@ -13,9 +14,15 @@ std::vector<unsigned int> TravelingSalesmanProblemGenerator::operator()() const
 {
     std::random_device rd;
     std::mt19937 g(rd());
-    std::vector<unsigned int> population(m_citiesCount);
-    std::iota (std::begin(population), std::end(population), 0);
-    std::shuffle (std::begin(population), std::end(population), g);
+    std::vector<unsigned int> solution(m_citiesCount);
+    std::iota (std::begin(solution), std::end(solution), 0);
+    std::shuffle (std::begin(solution), std::end(solution), g);
 
-    return population;
+    std::cout << "Generated solution: {";
+    for (unsigned int v: solution) {
+        std::cout << v << ' ';
+    }
+    std::cout << "}" << std::endl;
+
+    return solution;
 }
