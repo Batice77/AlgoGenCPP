8ef046504054e492a8bb3c3222828920ee880eee
Angèle <nicolas.besso@aliceadsl.fr>
2020-04-03 11:09:42 +0200

    update VCO
    pour ne pas mettre 2 fois le même élément

--- AlgoGenCPP/VoyagerCrossOperator.h
+++ AlgoGenCPP/VoyagerCrossOperator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 class VoyagerCrossOperator
 {
@@ -8,7 +9,6 @@ public :
 
 	std::vector<unsigned int> operator()(std::vector<unsigned int> list1, std::vector<unsigned int> list2) const {
 		std::vector<unsigned int> res;
-
 		int lim1 = list1.size() / 2;
 
 		for (int i = 0; i < lim1; i++) {
@@ -16,6 +16,26 @@ public :
 		}
 		for (unsigned int i = lim1; i < list2.size(); i++) {
 			res.push_back(list2[i]);
+		}
+
+		std::vector<unsigned int> values = list2;
+		std::vector<unsigned int> verif;
+		for (int i = 0; i < res.size(); i++) {
+			while(std::find(verif.begin(),verif.end(),values[values.size()]) != verif.end())
+			{
+				//tant qu'on a une value deja presente
+				values.pop_back();
+			}
+
+			if (std::find(verif.begin(), verif.end(), res[i]) != verif.end())
+			{
+				//ELEMENT FOUND
+				res[i] = values[values.size()];
+			}
+			else
+			{
+				verif.push_back(res[i]);
+			}
 		}
 		return res;
 	}
